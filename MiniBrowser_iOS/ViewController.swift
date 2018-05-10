0aac7716ea4bb7a7708e9eedf33e25a27737299c
llHoYall <hoya128@gmail.com>
2018-05-10 15:36:05 +0900

    Connect to interface builder

--- MiniBrowser_iOS/ViewController.swift
+++ MiniBrowser_iOS/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+	@IBOutlet weak var bookMarkSegCtrl: UISegmentedControl!
+	@IBOutlet weak var urlTextField: UITextField!
+	@IBOutlet weak var webView: WKWebView!
+	@IBOutlet weak var spinningInd: UIActivityIndicatorView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,19 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	@IBAction func bookMarkAction(_ sender: Any) {
+	}
+	
+	@IBAction func backAction(_ sender: Any) {
+	}
+	
+	@IBAction func forwardAction(_ sender: Any) {
+	}
+	
+	@IBAction func stopAction(_ sender: Any) {
+	}
+	
+	@IBAction func refreshAction(_ sender: Any) {
+	}
 }
-
