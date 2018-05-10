5bb7c2f2812c11543d7ac1fe88ffa725289a89c6
llHoYall <hoya128@gmail.com>
2018-05-10 17:30:05 +0900

    Complete application

--- MiniBrowser_iOS/ViewController.swift
+++ MiniBrowser_iOS/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
 	@IBOutlet weak var bookMarkSegCtrl: UISegmentedControl!
 	@IBOutlet weak var urlTextField: UITextField!
 	@IBOutlet weak var webView: WKWebView!
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		let bookmark = bookMarkSegCtrl.titleForSegment(at: bookMarkSegCtrl.selectedSegmentIndex)
+		let url = "https://www.\(bookmark!).com"
+		webView.navigationDelegate = self
+		webView.load(URLRequest(url: URL(string: url)!))		
+		urlTextField.text = url
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -26,17 +32,67 @@ class ViewController: UIViewController {
 	}
 	
 	@IBAction func bookMarkAction(_ sender: Any) {
+		let bookmark = bookMarkSegCtrl.titleForSegment(at: bookMarkSegCtrl.selectedSegmentIndex)
+		let url = "https://www.\(bookmark!).com"
+		webView.load(URLRequest(url: URL(string: url)!))
+		urlTextField.text = url
 	}
 	
 	@IBAction func backAction(_ sender: Any) {
+		if webView.canGoBack {
+			urlTextField.text = webView.backForwardList.backItem?.url.absoluteString
+			if urlTextField.text?.range(of: "facebook") != nil {
+				bookMarkSegCtrl.selectedSegmentIndex = 0
+			} else if urlTextField.text?.range(of: "google") != nil {
+				bookMarkSegCtrl.selectedSegmentIndex = 1
+			}	else if urlTextField.text?.range(of: "apple") != nil {
+				bookMarkSegCtrl.selectedSegmentIndex = 2
+			} else if urlTextField.text?.range(of: "twitter") != nil {
+				bookMarkSegCtrl.selectedSegmentIndex = 3
+			}
+			webView.goBack()
+		}
 	}
 	
 	@IBAction func forwardAction(_ sender: Any) {
+		if webView.canGoForward {
+			urlTextField.text = webView.backForwardList.forwardItem?.url.absoluteString
+			if urlTextField.text?.range(of: "facebook") != nil {
+				bookMarkSegCtrl.selectedSegmentIndex = 0
+			} else if urlTextField.text?.range(of: "google") != nil {
+				bookMarkSegCtrl.selectedSegmentIndex = 1
+			}	else if urlTextField.text?.range(of: "apple") != nil {
+				bookMarkSegCtrl.selectedSegmentIndex = 2
+			} else if urlTextField.text?.range(of: "twitter") != nil {
+				bookMarkSegCtrl.selectedSegmentIndex = 3
+			}
+			webView.goForward()
+		}
 	}
 	
 	@IBAction func stopAction(_ sender: Any) {
+		webView.stopLoading()
 	}
 	
 	@IBAction func refreshAction(_ sender: Any) {
+		webView.reload()
+	}
+	
+	func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+		spinningInd.startAnimating()
+	}
+	
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+		spinningInd.stopAnimating()
+	}
+	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		var url = "\(urlTextField.text!)"
+		if !url.hasPrefix("https://") {
+			url = "https://\(urlTextField.text!)"
+		}
+		webView.load(URLRequest(url: URL(string: url)!))
+		textField.resignFirstResponder()
+		return true
 	}
 }
